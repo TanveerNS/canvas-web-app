148bd5481f0379dbfa4d27ddc8205a411ff23b2b
tanveerns <tanveeralam229@gmail.com>
2022-11-17 02:55:11 +0530

    002

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ window.addEventListener('load', function () {
     const ctx = canvas.getContext('2d');
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
+    const ParticleArray = [];
 
     window.addEventListener('resize', function () {
         canvas.width = window.innerWidth;
@@ -17,30 +18,56 @@ window.addEventListener('load', function () {
     canvas.addEventListener('click', (event) => {
         mouse.x = event.x;
         mouse.y = event.y;
-        drawCircle()
     })
 
     canvas.addEventListener('mousemove', (event) => {
         mouse.x = event.x;
         mouse.y = event.y;
-        drawCircle()
     })
 
-    function drawCircle() {
-        ctx.fillStyle = 'blue';
-        ctx.beginPath()
-        ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2)
-        ctx.fill()
+    class Particle {
+        constructor() {
+            // this.x = mouse.x;
+            // this.y = mouse.y;
+            this.x = Math.random() * canvas.width;
+            this.y = Math.random() * canvas.height;
+            this.size = Math.random() * 5 + 1;
+            this.speedX = Math.random() * 3 - 1.5;
+            this.speedY = Math.random() * 3 - 1.5;
+        }
+        update() {
+            this.x += this.speedX;
+            this.y += this.speedY
+        }
+        draw() {
+            ctx.fillStyle = 'blue';
+            ctx.beginPath()
+            ctx.arc(this.x, this.y, 50, 0, Math.PI * 2)
+            ctx.fill()
+        }
     }
-    
+
+    function init(){
+        for(let i=0; i<100; i++){
+            ParticleArray.push(new Particle())
+        }
+    }
+
+    init()
+
+    function handleParticles(){
+        for(let i=0; i < ParticleArray.length; i++){
+            ParticleArray[i].update()
+            ParticleArray[i].draw()
+        }
+    }
 
     function animate(){
         ctx.clearRect(0, 0, canvas.width, canvas.height)
-        drawCircle()
+        handleParticles()
         requestAnimationFrame(animate)
     }
 
     animate()
-
 
 });
